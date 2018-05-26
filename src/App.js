6c7bb7bd6cd16512ea36b5572c69459441af9a13
minami-choi <cmina2846@gmail.com>
2018-05-26 13:26:57 +0900

    Lists with .map

--- src/App.js
+++ src/App.js
@@ -3,28 +3,46 @@ import './App.css';
 import Moive from './Movie'
 
 
-const movieTitles = [
-  "Matrics",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
-]
+// const movieTitles = [
+//   "Matrics",
+//   "Full Metal Jacket",
+//   "Oldboy",
+//   "Star Wars"
+// ]
 
-const movieImages = [
-  "https://cdn.namuwikiusercontent.com/s/b7f0bd8736d9a7bdd3bffc9bd9cf20d2ca8016b3d198dd2fb354c2eb34382b1ca69114814b95ef28b4d4d9eca6f32a0aac8c4eea562b30dbabbf73e57cb73de715d09185821a46df7fb68a7cc2cd7394?e=1529190443&k=_I2LpkY1sWZLVpYQmnLAEw",
-  "http://www.skolice.me/wp-content/uploads/2017/12/DAH-%C5%BDIVOTA-712x1024.jpg",
-  "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg",
-  "http://www.skolice.me/wp-content/uploads/2017/12/Star-Wars-Posljednji-d%C5%BEedaji-2-e1513171731333.jpg"
+// const movieImages = [
+//   "https://cdn.namuwikiusercontent.com/s/b7f0bd8736d9a7bdd3bffc9bd9cf20d2ca8016b3d198dd2fb354c2eb34382b1ca69114814b95ef28b4d4d9eca6f32a0aac8c4eea562b30dbabbf73e57cb73de715d09185821a46df7fb68a7cc2cd7394?e=1529190443&k=_I2LpkY1sWZLVpYQmnLAEw",
+//   "http://www.skolice.me/wp-content/uploads/2017/12/DAH-%C5%BDIVOTA-712x1024.jpg",
+//   "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg",
+//   "http://www.skolice.me/wp-content/uploads/2017/12/Star-Wars-Posljednji-d%C5%BEedaji-2-e1513171731333.jpg"
+// ]
+
+const movies = [
+  {
+    title: "Matrics",
+    poster: "https://cdn.namuwikiusercontent.com/s/b7f0bd8736d9a7bdd3bffc9bd9cf20d2ca8016b3d198dd2fb354c2eb34382b1ca69114814b95ef28b4d4d9eca6f32a0aac8c4eea562b30dbabbf73e57cb73de715d09185821a46df7fb68a7cc2cd7394?e=1529190443&k=_I2LpkY1sWZLVpYQmnLAEw",
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "http://www.skolice.me/wp-content/uploads/2017/12/DAH-%C5%BDIVOTA-712x1024.jpg",
+  },
+  {
+    title: "Oldboys",
+    poster: "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg",
+  },
+  {
+    title: "Star Wars",
+    poster: "http://www.skolice.me/wp-content/uploads/2017/12/Star-Wars-Posljednji-d%C5%BEedaji-2-e1513171731333.jpg"
+  }
 ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Moive title={movieTitles[0]} poster={movieImages[0]}/>
-        <Moive title={movieTitles[1]} poster={movieImages[1]}/>
-        <Moive title={movieTitles[2]} poster={movieImages[2]}/>
-        <Moive title={movieTitles[3]} poster={movieImages[3]}/>
+        {movies.map(movie => {
+         return <Moive title={movie.title} poster={movie.poster}/>
+        })}
       </div>
     );
   }
