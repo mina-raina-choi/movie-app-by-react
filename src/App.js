c72450261c7a1aca921b99c1b81da32f73f9e8e4
minami-choi <cmina2846@gmail.com>
2018-05-27 22:28:25 +0900

    Ajax in React

--- src/App.js
+++ src/App.js
@@ -16,30 +16,7 @@ class App extends Component {
   state = { }
   
   componentDidMount() {
-    setTimeout(() => {
-      // 절대 직접적으로 state변경하지말고 setState를 사용해서
-      // state를 변경하면 render()가 다시 실행됨.
-      this.setState({
-        movies : [
-          {
-            title: "Matrics",
-            poster: "https://cdn.namuwikiusercontent.com/s/b7f0bd8736d9a7bdd3bffc9bd9cf20d2ca8016b3d198dd2fb354c2eb34382b1ca69114814b95ef28b4d4d9eca6f32a0aac8c4eea562b30dbabbf73e57cb73de715d09185821a46df7fb68a7cc2cd7394?e=1529190443&k=_I2LpkY1sWZLVpYQmnLAEw",
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "http://www.skolice.me/wp-content/uploads/2017/12/DAH-%C5%BDIVOTA-712x1024.jpg",
-          },
-          {
-            title: "Oldboys",
-            poster: "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg",
-          },
-          {
-            title: "Star Wars",
-            poster: "http://www.skolice.me/wp-content/uploads/2017/12/Star-Wars-Posljednji-d%C5%BEedaji-2-e1513171731333.jpg"
-          }
-        ]
-      })
-    }, 2000);
+   console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count'))
   }
 
   _renderMovies = () => {
--- src/Movie.js
+++ src/Movie.js
@@ -2,38 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-// class Movie extends Component {
-
-//   // 부모컴포넌트에서 전달되는 데이터, 데이터타입을 체크
-//   static propTypes = {
-//     title: PropTypes.string.isRequired,
-//     poster: PropTypes.string.isRequired,
-//   }
-
-//   render() {
-//     // console.log(this.props)
-//     return (
-//       <div>
-//         <MoviePoster poster={this.props.poster}/>
-//         <h1>{this.props.title}</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// class MoviePoster extends Component {
-//   static propTypes = {
-//     poster: PropTypes.string.isRequired,
-//   }
-
-//   render() {
-//     console.log(this.props)
-//     return (
-//       <img src={this.props.poster} />
-//     )
-//   }
-// }
-
 // 어떤 컴포넌트는 단순히 return 하기위해 존재.
 // didmount, updatestate...이런 기능이 필요없음.
 // 그냥 한개의 props만 있으면 됨. 1개의 html태그만 필요
